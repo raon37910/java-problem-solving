3246fd256cd26d5910b24789d43c69a398c8c9bd
raon37910 <raon37910@gmail.com>
2024-11-15 05:47:05 +0900

    Title: 입력받은 값 교체하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+}
