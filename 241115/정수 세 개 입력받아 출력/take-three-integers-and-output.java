e4992a3a48be248867469786ff6a6bdf10496cf7
raon37910 <raon37910@gmail.com>
2024-11-15 05:51:51 +0900

    Title: 정수 세 개 입력받아 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.printf("%d %d %d",a ,b ,c);
+    }
+}
