c90d92beaa6b311208f67b09a4a922862413edcf
raon37910 <raon37910@gmail.com>
2024-11-15 05:41:23 +0900

    Title: 점수 입출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        System.out.printf("Your score is %d point.", a);
+    }
+}
