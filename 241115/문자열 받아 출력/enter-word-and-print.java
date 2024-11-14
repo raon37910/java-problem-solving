0d354b4edf1738f3c10454e1104907bf24161689
raon37910 <raon37910@gmail.com>
2024-11-15 05:54:26 +0900

    Title: 문자열 받아 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String c = sc.next();
+        System.out.println(c);
+    }
+}
