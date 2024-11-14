bdbdacd74081cbf6822bc7d23650b5d3f10d8ce7
raon37910 <raon37910@gmail.com>
2024-11-15 05:48:59 +0900

    Title: 입력받아 출력, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241115/입력받아 출력/input-and-output.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + " " + b);
+    }
+}
