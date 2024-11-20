4fb25e971f51ec4a731c968980155c608ff841b1
raon37910 <raon37910@gmail.com>
2024-11-20 21:36:25 +0900

    Title: 문자열 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
