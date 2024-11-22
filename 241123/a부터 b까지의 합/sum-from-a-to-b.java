035b279db8e88a4fc91c0097a18c25e20124e9e2
raon37910 <raon37910@gmail.com>
2024-11-23 00:32:07 +0900

    Title: a부터 b까지의 합, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241123/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        for(int i = a; i <= b; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
