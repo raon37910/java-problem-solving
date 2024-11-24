79d55101a1bcfadc871876447c8e89967d473f7a
raon37910 <raon37910@gmail.com>
2024-11-24 20:38:28 +0900

    Title: 조건에 따른 연산, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241124/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while(n < 1000) {
+            if(n % 2 == 0) {
+                n = n * 3 + 1;
+            } else {
+                n = n * 2 + 2;
+            }
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
