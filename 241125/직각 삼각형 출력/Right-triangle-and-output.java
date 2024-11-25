ac6fade59223ce961c1277c025ca9f06935fcf5d
raon37910 <raon37910@gmail.com>
2024-11-25 09:31:59 +0900

    Title: 직각 삼각형 출력, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241125/직각 삼각형 출력/Right-triangle-and-output.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= 2*i -1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
