403e5e8629b3354f46344ec94b3c24445254b392
raon37910 <raon37910@gmail.com>
2024-11-29 13:34:57 +0900

    Title: 트로미노, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241129/트로미노/tromino.java
+++ 241129/트로미노/tromino.java
@@ -13,7 +13,7 @@ public class Main {
         arr = new int[n][m];
 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < m; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
