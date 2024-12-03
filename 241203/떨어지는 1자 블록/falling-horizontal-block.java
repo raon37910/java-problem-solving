e419ccceca0736038ac9d832f66f2e4b58221cf9
raon37910 <raon37910@gmail.com>
2024-12-03 09:40:55 +0900

    Title: 떨어지는 1자 블록, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241203/떨어지는 1자 블록/falling-horizontal-block.java
+++ 241203/떨어지는 1자 블록/falling-horizontal-block.java
@@ -11,9 +11,10 @@ public class Main {
     public static void main(String[] args) {
         input();
 
-        for(int row = n; row >= 1; row--) {
+        for(int row = 1; row <= n-1; row++) {
             boolean flag = true;
 
+            // 비어 있어서 내려 갈 수 있는지?
             for(int col = k; col <= k + m - 1; col++) {
                 if(arr[row][col] == 1) {
                     flag = false;
@@ -21,11 +22,29 @@ public class Main {
                 }
             }
 
+            // 비어 있어서 내려 갈 수 있다면
             if(flag) {
+                boolean end = false;
+                // 다음칸에 접근을 못 해서 멈춰야 하는지?
                 for(int col = k; col <= k + m - 1; col++) {
-                    arr[row][col] = 1;
+                    if(arr[row + 1][col] == 1) {
+                        end = true;
+                        break;
+                    }
                 }
-                break;
+
+                if(end) {
+                    for(int col = k; col <= k + m - 1; col++) {
+                        arr[row][col] = 1;
+                    }
+                    break;
+                }
+                // row = n - 1 일때 end가 False라면 바닥이라는 뜻임
+                if (row == n - 1) {
+                    for(int col = k; col <= k + m - 1; col++) {
+                        arr[row + 1][col] = 1;
+                    }
+                }
             }
         }
 
